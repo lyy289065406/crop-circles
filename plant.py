8dc9af344af8e9c203085e3f6e38c88d28b2fdb7
EXP <exp.lqb@foxmail.com>
2019-08-02 18:42:47 +0800

    增加种草概率

--- plant.py
+++ plant.py
@@ -2,13 +2,17 @@
 # -*- coding: utf-8 -*-
 
 import os
+import random
 import time
 import git
 import uuid
 
-REPO_DIR = os.path.split(os.path.realpath(__file__))[0]
-GRASSLAND = '%s/grassland.dat' % REPO_DIR
-KEEP_SIZE = 1 * 1024 * 1024     # unit: M
+
+REPO_DIR = os.path.split(os.path.realpath(__file__))[0]     # 仓库目录
+GRASSLAND = '%s/grassland.dat' % REPO_DIR                   # 种草文件
+KEEP_SIZE = 1 * 1024 * 1024                                 # 种草文件大小，单位: M
+PROBABILITY = 80                                            # 种草概率
+
 
 
 def main() :
@@ -17,26 +21,36 @@ def main() :
     to_github(now)
 
 
+# 获取当前系统时间
 def get_systime() :
     time_obj = time.localtime(time.time())
     return time.strftime("%Y-%m-%d %H:%M:%S", time_obj)
 
 
+# 种草
 def to_plant(systime) :
-    fsize = 0
-    if os.path.exists(GRASSLAND) :
-        fsize = os.path.getsize(GRASSLAND)
-    mode = 'a+' if KEEP_SIZE > fsize else 'w+'
+    if is_bingo() :
+        fsize = 0
+        if os.path.exists(GRASSLAND) :
+            fsize = os.path.getsize(GRASSLAND)
+        mode = 'a+' if KEEP_SIZE > fsize else 'w+'
 
-    with open(GRASSLAND, mode) as file :
-        file.write('%s\n' % systime)
+        with open(GRASSLAND, mode) as file :
+            file.write('%s\n' % systime)
 
 
+# 是否命中种草概率
+def is_bingo() :
+    return random.randint(0,100) <= PROBABILITY
+
+
+# 提交到 github
 def to_github(systime) :
     repo = git.Repo(REPO_DIR)
     repo.git.add(GRASSLAND)
     repo.git.commit(m='[%s] %s' % (systime, uuid.uuid1()))
     repo.git.push()
+
 
 
 if __name__ == '__main__' :

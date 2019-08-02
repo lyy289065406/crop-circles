d405b9bbf5f9f7100c2f41d4bbd1a4ccc57468c0
EXP <exp.lqb@foxmail.com>
2019-08-02 17:23:37 +0800

    1.0

---
+++ plant.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# pip install GitPython
+# https://www.cnblogs.com/baiyangcao/p/gitpython.html
+
+
+# crontab -> this.py : 
+# 1. now(datetime) to grassland 
+# 2. git push
+
+
+import time
+import git
+import uuid
+
+GRASSLAND = './grassland.dat'
+
+
+def main() :
+    now = get_systime()
+    to_plant(now)
+    to_github(now)
+
+
+def get_systime() :
+    time_obj = time.localtime(time.time())
+    return time.strftime("%Y-%m-%d %H:%M:%S", time_obj)
+
+
+def to_plant(systime) :
+    with open(GRASSLAND, 'a+') as file :
+        file.write('%s\n' % systime)
+
+
+def to_github(systime) :
+    repo = git.Repo(r'.')
+    repo.git.add(GRASSLAND)
+    repo.git.commit(m='[%s] %s' % (systime, uuid.uuid1()))
+    repo.git.push()
+
+
+if __name__ == '__main__' :
+    main()
+

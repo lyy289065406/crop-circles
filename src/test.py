0d0293f6d899ef0659149002e4a1443c4722c3f7
EXP <289065406@qq.com>
2020-08-01 02:45:58 +0800

    letters

--- src/test.py
+++ src/test.py
@@ -12,6 +12,13 @@ CANVAS_HEIGHT = 7
 WHITE = '□'
 BLACK = '■'
 
+tables = {
+    'A': A, 'B': B, 'C': C, 'D': D, 'E': E, 'F': F, 'G': G, 'H': H, 'I': I, 'J': J, 
+    'K': K, 'L': L, 'M': M, 'N': N, 'O': O, 'P': P, 'Q': Q, 'R': R, 'S': S, 'T': T, 
+    'U': U, 'V': V, 'W': W, 'X': X, 'Y': Y, 'Z': Z, '0': zero, '1': one, '2': two, 
+    '3': three, '4': four, '5': five, '6': six, '7': seven, '8': eight, '9': night
+}
+
 
 def init_canvas(row=CANVAS_HEIGHT, col=CANVAS_WIDTH, default=WHITE):
     canvas = [ None ] * row
@@ -22,9 +29,21 @@ def init_canvas(row=CANVAS_HEIGHT, col=CANVAS_WIDTH, default=WHITE):
 
 def to_index(str):
     index = list(str)
+    return index
 
-def to_matrix(index):
-    pass
+
+def fill_canvas(canvas, index):
+    offset = 0
+    for ch in index :
+        obj = tables.get(ch)
+
+        for r in range(0, 7) :
+            for c in range(0, 7) :
+                if obj[r][c] == 1 :
+                    canvas[r][c + offset] = BLACK
+        offset += 7
+
+
 
 
 def to_print(canvas) :
@@ -38,5 +57,7 @@ def to_print(canvas) :
 
 if __name__ == '__main__' :
     canvas = init_canvas()
+    index = to_index("789")
+    fill_canvas(canvas, index)
     to_print(canvas)
 

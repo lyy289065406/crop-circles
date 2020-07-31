4dc09744b4c64388d460827a8698911bc72162ae
EXP <exp.lqb@foxmail.com>
2020-07-31 14:03:46 +0800

    canvas

--- src/test.py
+++ src/test.py
@@ -6,17 +6,37 @@
 # FIXME
 from letter import *
 
+
+CANVAS_WIDTH = 53
+CANVAS_HEIGHT = 7
 WHITE = '□'
 BLACK = '■'
 
 
+def init_canvas(row=CANVAS_HEIGHT, col=CANVAS_WIDTH, default=WHITE):
+    canvas = [ None ] * row
+    for r in range(len(canvas)):  
+        canvas[r] = [ default ] * col
+    return canvas
 
-def to_print(matrix) :
-    for i in matrix :
+
+def to_index(str):
+    index = list(str)
+
+def to_matrix(index):
+    pass
+
+
+def to_print(canvas) :
+    for i in canvas :
         for j in i :
-            print('%s ' % (WHITE if j == 0 else BLACK), end='')
+            print(j, end=' ')
         print()
 
 
+
+
 if __name__ == '__main__' :
-    to_print(night)
+    canvas = init_canvas()
+    to_print(canvas)
+

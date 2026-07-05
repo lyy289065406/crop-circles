66577886d192ae4d1457090b3cb159756d97e86e
EXP <289065406@qq.com>
2026-07-06 01:16:38 +0800

    Update dot_matrix.py

--- src/env/dot_matrix.py
+++ src/env/dot_matrix.py
@@ -75,15 +75,15 @@ r_title =       [ [ 0, 0, 0 ], [ 1, 0, 0 ], [ 0, 1, 0 ], [ 0, 0, 1 ], [ 0, 1, 0 
 
 
 DOT_MATRIX = {
-    ('A', A), ('B', B), ('C', C), ('D', D), ('E', E), ('F', F), ('G', G), ('H', H), ('I', I), 
-    ('J', J), ('K', K), ('L', L), ('M', M), ('N', N), ('O', O), ('P', P), ('Q', Q), ('R', R), 
-    ('S', S), ('T', T),  ('U', U), ('V', V), ('W', W), ('X', X), ('Y', Y), ('Z', Z),  
-    ('0', zero), ('1', one), ('2', two), ('3', three), ('4', four), ('5', five),  
-    ('6', six), ('7', seven), ('8', eight), ('9', night),  
-    (' ', space), ('·', point), ('.', period), (',', comma), (';', semicolon), (':', colon),  
-    ('?', question), ('!', exclamation), ('$', doller), ('^', power), ('*', star),  
-    ('=', equal), ('+', plus), ('-', minus), ('_', underline), 
-    ('\'', apos),  ('|', vertical), ('/', slash), ('\\', backslash),  
-    ('(', l_parentheses), (')', r_parentheses),  ('[', l_brackets), (']', r_brackets),  
-    ('{', l_braces), ('}', r_braces), ('<', l_title), ('>', r_title)
+    'A': A, 'B': B, 'C': C, 'D': D, 'E': E, 'F': F, 'G': G, 'H': H, 'I': I, 
+    'J': J, 'K': K, 'L': L, 'M': M, 'N': N, 'O': O, 'P': P, 'Q': Q, 'R': R, 
+    'S': S, 'T': T, 'U': U, 'V': V, 'W': W, 'X': X, 'Y': Y, 'Z': Z,  
+    '0': zero, '1': one, '2': two, '3': three, '4': four, '5': five,  
+    '6': six, '7': seven, '8': eight, '9': night,  
+    ' ': space, '·': point, '.': period, ',': comma, ';': semicolon, ':': colon,  
+    '?': question, '!': exclamation, '$': doller, '^': power, '*': star,  
+    '=': equal, '+': plus, '-': minus, '_': underline, 
+    '\'': apos, '|': vertical, '/': slash, '\\': backslash,  
+    '(': l_parentheses, ')': r_parentheses, '[': l_brackets, ']': r_brackets,  
+    '{': l_braces, '}': r_braces, '<': l_title, '>': r_title
 }
